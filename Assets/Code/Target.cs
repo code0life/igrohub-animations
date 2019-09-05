385fa91e0e13b62ee3d95c10aec30c911735ef5e
ES\vadim.levashko <code0life@gmail.com>
2019-09-05 13:36:53 +0300

    add

---
+++ Assets/Code/Target.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+  void OnTriggerEnter(Collider other)
+  {
+    bool is_player = other.gameObject.GetComponent<CharacterControl>() != null;
+    if(is_player)
+    {
+      AddScore();
+      Destroy(gameObject);
+    }
+  }
+
+  void AddScore()
+  {
+    GameObject.FindObjectOfType<ScoreCounter>().Add(1);
+  }
+}
